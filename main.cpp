214d4881c328bba1f1a95fb9ac8355e3fe431aed
SeriousHyena <emackiii@gmail.com>
2021-10-29 14:00:29 -0700

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class House {
+public:
+	void setNumStories(int numStories) {
+		this->numStories = numStories;
+	}
+
+	void setNumWindows(int numWindows) {
+		this->numWindows = numWindows;
+	}
+
+	void setColor(string color) {
+		this->color = color;
+	}
+
+	int getNumStories() const {
+		return numStories;
+	}
+
+	int getNumWindows() const {
+		return numWindows;
+	}
+
+	string getColor() const {
+		return color;
+	}
+
+private:
+
+	int numStories;
+	int numWindows;
+	string color;
+};
+
 int main() {
+
+	House myHouse;
+	House yourHouse;
+
+	myHouse.setNumStories(2);
+	myHouse.setNumWindows(6);
+	myHouse.setColor("Red");
+
+	yourHouse.setNumStories(3);
+	yourHouse.setNumWindows(10);
+	yourHouse.setColor("Blue");
+
+	cout << "My house has " << myHouse.getNumStories() << " stories and has " << myHouse.getNumWindows() << " windows and is " << myHouse.getColor()<<endl;
+	cout << "Your house has " << yourHouse.getNumStories() << " stories and has " << yourHouse.getNumWindows() << " windows and is " << yourHouse.getColor();
+
 
 	return 0;
 }
